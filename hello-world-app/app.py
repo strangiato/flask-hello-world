9617ff379466fdd80c875b3b3b310ef7090b700a
Trevor Royer <troyer@redhat.com>
2022-03-01 13:27:16 -0600

    add app start

--- hello-world-app/app.py
+++ hello-world-app/app.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+
+    host_ip = "0.0.0.0"  # noqa: S104
+    host_port = "5000"
+
+    app.run(host=host_ip, port=host_port)
